92d33673b9bf2f38f977638c34458a9045cc097f
halx99 <halx99@live.com>
2020-07-03 22:20:24 +0800

    Disable opengl on win32

--- build.bat
+++ build.bat
@@ -4,11 +4,14 @@ rem ---------- setup env
 set programdir=%ProgramFiles%
 if exist "%ProgramFiles% (x86)" set programdir=%programdir% (x86)
 call "%programdir%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+rem call "%programdir%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 
 rem --- 1. angle_enable_vulkan=false
 rem --- 2. dynamic crt MD (angle\build\config\win\BUILD.gn)
 pushd angle
-call gn gen out/release --sln=angle-release --ide=vs2019 "--args=is_debug=false is_clang=false target_cpu=\"x86\" treat_warnings_as_errors=false angle_enable_vulkan=false"
+rem git pull
+rem gclient sync -D
+call gn gen out/release --sln=angle-release --ide=vs2019 "--args=is_debug=false is_clang=false target_cpu=\"x86\" treat_warnings_as_errors=false angle_enable_vulkan=false angle_enable_gl=false"
 call devenv out\release\angle-release.sln /rebuild "GN|Win32" /Project libEGL
 popd
 
